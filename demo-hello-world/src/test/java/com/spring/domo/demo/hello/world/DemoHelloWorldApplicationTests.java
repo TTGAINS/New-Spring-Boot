04c0ac15ee46705ab7cfb165c4bfb87f924c7167
Tebatso Thakhisi <mosimanegapethakhisi@gmail.com>
2022-06-16 15:40:26 +0200

    My First Spring

---
+++ demo-hello-world/src/test/java/com/spring/domo/demo/hello/world/DemoHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.domo.demo.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
